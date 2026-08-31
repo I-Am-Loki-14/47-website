c75b5ba111c9749b9122257849e67b8f5749b2bc
Min Naing Oo <minnaingoo792@gmail.com>
2026-09-01 01:06:38 +0630

    Thai47/Chinese47-menu_cards-update

--- js/javascript.js
+++ js/javascript.js
@@ -29,28 +29,62 @@ function dark() {
       
     }
 }
-// Tiny Js 
+
 const DTcard = document.getElementById('dt-card');
 const Th47 = document.getElementById('thai47')
-console.log(DTcard);
+
 
 const CHcard = document.getElementById('ch-card')
 const Ch47 = document.getElementById('chinese47')
-console.log(CHcard);
 
 
-function T47() {
+
+
+
+
+  // Thai 47 Js
+        const MenuSlidersOne = document.querySelectorAll('.menu-container-1 .menu-container-card')
+        let ThaiIndex = 0
+
+        function nextthai() {
+            ThaiIndex = (ThaiIndex + 1) % MenuSlidersOne.length
+            MenuSlidersOne.forEach(slide => slide.classList.remove('active-first'));
+            MenuSlidersOne[ThaiIndex].classList.add('active-first')
+        }
+        function prevthai() {
+            ThaiIndex = (ThaiIndex - 1 + MenuSlidersOne.length) % MenuSlidersOne.length
+            MenuSlidersOne.forEach(slide => slide.classList.remove('active-first'))
+            MenuSlidersOne[ThaiIndex].classList.add('active-first')
+        }
+        // Chinese 47 Js
+        const MenuSlidersTwo = document.querySelectorAll('.menu-container-2 .menu-container-card')
+        let ChineseIndex = 0
+
+        function nextchinese() {
+            ChineseIndex = (ChineseIndex + 1) % MenuSlidersTwo.length
+            MenuSlidersTwo.forEach(slide => slide.classList.remove('active-first'));
+            MenuSlidersTwo[ChineseIndex].classList.add('active-first')
+        }
+        function prevchinese() {
+            ChineseIndex = (ChineseIndex - 1 + MenuSlidersTwo.length) % MenuSlidersTwo.length
+            MenuSlidersTwo.forEach(slide => slide.classList.remove('active-first'))
+            MenuSlidersTwo[ChineseIndex].classList.add('active-first')
+        }
+        // setInterval(nextt, 4000)
+
+        function T47() {
+           
         DTcard.style.display = "block"
-       
         CHcard.style.display = "none"
         Th47.style.backgroundColor = "#ffa42cdf"
         Th47.style.color = "#000000e7"
-         Ch47.style.backgroundColor = "transparent"
+        Ch47.style.backgroundColor = "transparent";
+       
     }
     function CH47() {
+      
         Ch47.style.backgroundColor = "#ffa42cdf"
         CHcard.style.display = "block";
         DTcard.style.display = "none";
-         Th47.style.backgroundColor = "transparent"
-
+         Th47.style.backgroundColor = "transparent";
  }
