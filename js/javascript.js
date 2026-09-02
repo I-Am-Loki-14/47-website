98fd5d377e9d6b5f4a3381973c299e2f40059bd6
Min Naing Oo <minnaingoo792@gmail.com>
2026-09-02 12:29:29 +0630

    burmaMenu-add

--- js/javascript.js
+++ js/javascript.js
@@ -48,6 +48,9 @@ const CHcard = document.getElementById('ch-card')
 const Ch47 = document.getElementById('chinese47')
 
 
+     const BUcard = document.getElementById('bu-card')
+const Bu47 = document.getElementById('burma47')
+
 
 
 
@@ -81,14 +84,32 @@ const Ch47 = document.getElementById('chinese47')
             MenuSlidersTwo[ChineseIndex].classList.add('active-first')
         }
         // setInterval(nextt, 4000)
+// Burma47 Js
+        const MenuSlidersThree = document.querySelectorAll('.menu-container-3 .menu-container-card')
+        let BurmaIndex = 0
+
+        function nextburma() {
+             BurmaIndex = ( BurmaIndex + 1) % MenuSlidersThree.length
+            MenuSlidersThree.forEach(slide => slide.classList.remove('active-first'));
+            MenuSlidersThree[ BurmaIndex].classList.add('active-first')
+        }
+        function prevburma() {
+             BurmaIndex = ( BurmaIndex - 1 + MenuSlidersThree.length) % MenuSlidersThree.length
+            MenuSlidersThree.forEach(slide => slide.classList.remove('active-first'))
+            MenuSlidersThree[ BurmaIndex].classList.add('active-first')
+        }
+        // setInterval(nextt, 4000)
+
 
         function T47() {
            
         DTcard.style.display = "block"
+        BUcard.style.display = "none";
         CHcard.style.display = "none"
         Th47.style.backgroundColor = "#ffa42cdf"
         Th47.style.color = "#000000e7"
         Ch47.style.backgroundColor = "transparent";
+        Bu47.style.backgroundColor = "transparent"
        
     }
     function CH47() {
@@ -96,5 +117,17 @@ const Ch47 = document.getElementById('chinese47')
         Ch47.style.backgroundColor = "#ffa42cdf"
         CHcard.style.display = "block";
         DTcard.style.display = "none";
+        BUcard.style.display = "none";
          Th47.style.backgroundColor = "transparent";
- }
+         Bu47.style.backgroundColor = "transparent"
+            }
+
+             function BU47() {
+ 
+        Bu47.style.backgroundColor = "#ffa42cdf"
+        BUcard.style.display = "block";
+        DTcard.style.display = "none";
+        CHcard.style.display = "none";
+         Th47.style.backgroundColor = "transparent";
+         Ch47.style.backgroundColor = "transparent";
+            }
